c0147f876d73452826ccc81290fb24e93fd8b840
brian2606 <86303942+brian2606@users.noreply.github.com>
2021-12-07 18:58:56 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+player1name=localStorage.getItem("player1name");
+ player2name=localStorage.getItem("player2name");
+  player1score=0;
+ player2score=0;
+document.getElementById("player1name").innerHTML=player1name+"=";
+document.getElementById("player2name").innerHTML=player2name+"=";
+document.getElementById("player1score").innerHTML=player1score;
+document.getElementById("player2score").innerHTML=player2score;
+document.getElementById("playerque").innerHTML="question turn="+player1name;
+document.getElementById("playerans").innerHTML="answer turn="+player2name;
